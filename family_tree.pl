38e5d66c192a8916ae552039d87150564690c699
Glavin <glavinlobo92@gmail.com>
2025-04-29 01:50:19 +0530

    changes in family_tree.pl

--- family_tree.pl
+++ family_tree.pl
@@ -1,109 +1,45 @@
-mother(sangama, mathew).
-mother(sangama, chitra).
-mother(philomena, rita).
-mother(philomena, glady).
-mother(rita, manoah).
-mother(rita, gideon).
-mother(glady, carol).
-mother(chitra, deepak).
-mother(chitra, asha).
+mother(celestine, arun).
+mother(celestine, hilda).
+mother(mary, gregory).
+mother(mary, rita).
+mother(hilda, glavin).
+mother(hilda, glenn).
 
-father(manick, mathew).
-father(manick, chitra).
-father(fedrich, rita).
-father(fedrich, glady).
-father(mathew, manoah).
-father(mathew, gideon).
-father(shivraj, deepak).
-father(shivraj, asha).
-father(ronald, carol).
-
-parent(P, C) :-
-    mother(P, C);
-    father(P, C).
-
-siblings(X, Y) :-
-    parent(P, X),
-    parent(P, Y),
-    X \= Y.
-
-brother(B, S) :-
-    siblings(B, S),
-    male(B).
-
-sister(S, B) :-
-    siblings(S, B),
-    female(S).
-
-uncle(Uncle, Child) :-
-    parent(P, Child),
-    siblings(Uncle, P),
-    male(Uncle).
-
-aunt(Aunt, Child) :-
-    parent(P, Child),
-    siblings(Aunt, P),
-    female(Aunt).
-
-grandfather(GF, Child) :-
-    father(GF, P), 
-    parent(P, Child).
-
-grandmother(GM, Child) :-
-    mother(GM, P),
-    parent(P, Child).
-
-cousin(Cousin, Child) :-
-    parent(P1, Child),
-    parent(P2, Cousin),
-    siblings(P1, P2),
-    Child \= Cousin.
+father(marcel, arun).
+father(marcel, hilda).
+father(felix, gregory).
+father(felix, rita).
+father(gregory, glavin).
+father(gregory, glenn).
+father(arun, aston).
 
 
-male(mathew).
-male(deepak).
-male(manoah).
-male(gideon).
-male(manick).
-male(fedrich).
-male(shivraj).
-male(ronald).
 
-female(chitra).
-female(asha).
-female(rita).
-female(glady).
-female(carol).
-female(sangama).
-female(philomena).
+parent(M, F, C) :- 
+    (mother(M, C), father(F, C)).
+
+grandparent(GM, GF, C) :-
+    mother(M,C), mother(GM,M);
+	father(F,C), mother(GM,F);
+	mother(M,C), father(GF,M);
+	father(F,C), father(GF,F).
 
 
-/*
-1. Save this file with the extension `.pl`, e.g., family.pl
+sibling(C, S) :-
+    
+    (father(F,C),father(F,S)),
+     C \= S.
 
-2. Open SWI-Prolog (or any Prolog interpreter).
+uncle(U, C) :-
+    ((sibling(F, U), father(F, C)); 
+    (sibling(M, U), mother(M, C))),
+    M \= U. 
 
-3. Load the file by typing:
-   ?- [family].
+aunt(A, C) :-
+    ((sibling(F, A), father(F, C)); 
+    (sibling(M, A), mother(M, C))),
+    F \= A.
 
-4. Now you can make queries, for example:
-
-   % Find Manoah's parents
-   ?- parent(X, manoah).
-
-   % Find Manoah's siblings
-   ?- siblings(manoah, Sibling).
-
-   % Find who is Manoah's uncle
-   ?- uncle(Uncle, manoah).
-
-   % Find Manoah's grandparents
-   ?- grandfather(GF, manoah).
-   ?- grandmother(GM, manoah).
-
-   % Find who are Manoah's cousins
-   ?- cousin(Cousin, manoah).
-
-5. To exit, type:
-   ?- halt.
-*/
+cousin(A, U, C, CO) :-
+    (aunt(A, C), mother(A, CO)); 
+    (uncle(U, C), father(U, CO)).
